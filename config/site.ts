b85680c4c96d0409e2db720d12f5e12d7a59e1a2
Tolga Yaycı <40897846+tolgayayci@users.noreply.github.com>
2023-11-16 09:38:22 +0300

    fixed web

--- config/site.ts
+++ config/site.ts
@@ -1,13 +1,13 @@
 import { SiteConfig } from "types"
 
 export const siteConfig: SiteConfig = {
-  name: "Taxonomy",
+  name: "Yk Labs",
   description:
     "An open source application built using the new router, server components and everything new in Next.js 13.",
-  url: "https://tx.shadcn.com",
+  url: "https://yk-labs.com",
   ogImage: "https://tx.shadcn.com/og.jpg",
   links: {
-    twitter: "https://twitter.com/shadcn",
-    github: "https://github.com/shadcn/taxonomy",
+    twitter: "https://twitter.com/yk-labs",
+    github: "https://github.com/yk-labs",
   },
 }
